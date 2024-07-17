7c372f2a5b37b2a7d51c79e7cf7053c87a4f8ac2
SURAJ GUPTA <Suraj802119@gmail.com>
2024-07-18 01:42:54 +0530

    second commit

--- script.js
+++ script.js
@@ -31,10 +31,45 @@ next_btn.addEventListener('click', (e) => {
     changeSlide();
 })
 
-const scrollContainer = doucument.querySelectorAll('.products');
+//wheel scroller
+const scrollContainer = document.querySelectorAll('.products');
+
 for(const item of scrollContainer) {
     item.addEventListener('wheel', (evt)=>{
         evt.preventDefault();
         item.scrollLeft += evt.deltaY;
     })
 }
+
+//back to top button 
+
+const backtop = document.querySelector(".backtop");
+
+backtop.addEventListener('click', () => {
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    });
+})
+
+//sidebar
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.querySelector('.sidebar');
+    const cross = document.querySelector('.fa-xmark');
+    const sidebtn = document.querySelector('#menu_icon');
+
+    sidebtn.addEventListener('click', () => {
+        sidebar.classList.add('active');
+        cross.classList.add('active');
+    });
+    cross.addEventListener('click', () =>{
+        sidebar.classList.remove('active');
+        cross.classList.remove('active');
+    });
+});
+
+
+
+
+
